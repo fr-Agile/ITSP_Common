7bd5d95c05c0e7cb0b8de7ad69ab8e4ed7ea9fa4
Dai Yamamoto <gos415345@gmail.com>
2013-11-15 12:08:03 +0900

    繰り返し用の定数

---
+++ ITSP_Common/src/jp/ac/titech/itpro/sds/fragile/api/constant/RepeatConstant.java
@@ -0,0 +1,7 @@
+package jp.ac.titech.itpro.sds.fragile.api.constant;
+
+public class RepeatConstant {
+	// 繰り返し期間の指定が無かった場合の繰り返し期間(googleにexportする関係上、適当に区切る)
+	public static final long REPEAT_BEGIN = 946652400000L;	// 2000年から
+	public static final long REPEAT_END = 32503647600000L;	// 3000年まで
+}
